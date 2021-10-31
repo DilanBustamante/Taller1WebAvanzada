442ce3593c8d36c0749080d371848302835ddc44
Dilan Daniel Bustamante Martinez <1144209785@u.icesi.edu.co>
2021-10-31 02:46:45 -0500

    ChatV2

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,19 @@
 
   <v-app>
       <Principal/>
-      <Register/>
-      <Chat/>
   </v-app>
-  
+
 </template>
 
 <script>
 import Principal from './components/Principal.vue';
-import Register from './components/Register';
-import Chat from './components/Chat'
+
 
 
 export default {
   name: 'App',
   components: {
     Principal,
-    Register,
-    Chat
-    
     
   },
 
@@ -30,8 +24,3 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
- .v-application{
-    background: rgba(87, 189, 20, 0.11) !important;
- }
-</style>
