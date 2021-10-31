0886e86a63a72bf6ea76f2000059dd47603566ee
Daniela <jessot@hotmail.com>
2021-10-30 22:40:36 -0500

    adding login component

---
+++ src/components/Principal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Welcome to foro, please login or register.
+        <Login />
+    </div> 
+</template>
+<script>
+import Login from './Login.vue';
+export default {
+    name: 'Principal',
+    components: {
+      Login
+    }
+}
+</script>
