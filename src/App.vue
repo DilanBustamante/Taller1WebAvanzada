c4320bb3e74c6c301a80bf0d5d88bb722be611c0
Dilan Daniel Bustamante Martinez <1144209785@u.icesi.edu.co>
2021-10-31 02:41:53 -0500

    ChatV1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
+
   <v-app>
       <Principal/>
       <Register/>
+      <Chat/>
   </v-app>
+  
 </template>
 
 <script>
 import Principal from './components/Principal.vue';
 import Register from './components/Register';
+import Chat from './components/Chat'
 
 
 export default {
   name: 'App',
   components: {
     Principal,
-    Register
+    Register,
+    Chat
     
     
   },
---
+++ src/components/Chat.vue
@@ -0,0 +1,45 @@
+<template>
+
+        <v-col class="container"> 
+           
+            <div class="container-comment">
+                 <v-text-field  label ="Agrega tu comentario" filled class="comment"/>
+                 <a> Responder</a>
+            </div>
+             
+
+        </v-col>
+
+</template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+
+  .container{
+      margin: auto;
+      width: 1300px;
+  }
+
+
+  .comment{
+      width: 900px;
+      margin:auto;
+  }
+
+  .container-comment{
+      text-align: center;
+  }
+
+  a{
+      margin-right: 800px;
+  }
+
+</style>
